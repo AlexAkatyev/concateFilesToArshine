7924f1047886bb449c8da0ffcce1a496b5c60a27
Alex Akatyev <aleksey.akatyev@gmail.com>
2022-08-11 16:37:41 +0400

    Объединять файлы в один до 6М

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import xml.etree.ElementTree as ET
 
 
+large_size = 2000000
+max_size = 6000000
+
+
 def print_separator():
     print('------------------------------------------')
 
@@ -10,7 +14,6 @@ def print_separator():
 def is_small_xml(fileName):
     result = False
     pattern = '.xml'
-    large_size = 30000
     if fileName.endswith(pattern):
         file_stat = os.stat(fileName)
         if file_stat.st_size < large_size:
@@ -18,25 +21,43 @@ def is_small_xml(fileName):
     return result
 
 
+def get_new_name(file_name, counter):
+    return file_name + "-" + f'{counter}' + ".xml"
+
+
+def print_inc_and_del(file_xml, new_file_name, file_counter):
+    print(u'Файл ' + f'{file_xml}' + u' включен в ' + get_new_name(new_file_name, file_counter) + u' и удален')
+
+
 def concatenate_of_files(list_xml):
-    print(u'Начало объединения файлов')
-    trees = []
-    for file_xml in list_xml:
-        trees.append(ET.parse(file_xml))
     new_file_name = input(u'Введите имя объединеного файла (без расширения) : ')
-    new_file_name += ".xml"
-    gtree = trees[0]
-    groot = gtree.getroot()
-    i = 1
-    while i < len(trees):
-        root = trees[i].getroot()
-        for elem in root:
-            groot.append(elem)
-        i += 1
-    gtree.write(new_file_name, encoding="utf-8")
-    print(u'Файлы объединены в ' + f'{new_file_name}')
+    file_counter = 1;
+    print(u'Объединение файлов:')
+    while len(list_xml) > 0:
+        i = 0
+        file_xml = list_xml[i]
+        gtree = ET.parse(file_xml)
+        groot = gtree.getroot()
+        new_file_size = os.stat(file_xml).st_size
+        os.remove(list_xml.pop(i))
+        print_inc_and_del(file_xml, new_file_name, file_counter)
+        while i < len(list_xml):
+            file_xml = list_xml[i]
+            add_size = os.stat(file_xml).st_size
+            if new_file_size + add_size < max_size:
+                new_file_size += add_size
+                root = ET.parse(file_xml).getroot()
+                for elem in root:
+                    groot.append(elem)
+                os.remove(list_xml.pop(i))
+                print_inc_and_del(file_xml, new_file_name, file_counter)
+            else:
+                i += 1
+        gtree.write(get_new_name(new_file_name, file_counter), encoding="utf-8")
+        file_counter += 1
 
 
+#  not in use
 def delete_files(list_of_files):
     answer = input(u'Удалить обработанные файлы? y/n(н/т) ')
     if answer == 'y' or answer == 'Y' or answer == u'Н' or answer == u'н':
@@ -60,8 +81,6 @@ if __name__ == '__main__':
     print(u'Всего файлов : ' + f'{lenOfFiles}')
     if lenOfFiles > 0:
         concatenate_of_files(listOfFiles)
-        print_separator()
-        delete_files(listOfFiles)
     else:
         print(u'Файлы для обработки отсутствуют')
     print_separator()
